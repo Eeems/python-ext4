429a86838519ee36104f6b649b1ef7c9684d4033
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2024-05-25 23:02:53 -0600

    Add testing

---
+++ test.py
@@ -0,0 +1,31 @@
+import sys
+import ext4
+
+FAILED = False
+
+
+def test_path_tuple(path, expected):
+    print(f"check Volume.path_tuple({path}): ", end="")
+    try:
+        t = ext4.Volume.path_tuple(path)
+        if t != expected:
+            raise ValueError(f"Result is unexpected {t}")
+
+        print("pass")
+
+    except Exception as e:
+        FAILED = True
+        print("fail")
+        print("  ", end="")
+        print(e)
+
+
+test_path_tuple("/", tuple())
+test_path_tuple(b"/", tuple())
+test_path_tuple("/test", (b"test",))
+test_path_tuple(b"/test", (b"test",))
+test_path_tuple("/test/test", (b"test", b"test"))
+test_path_tuple(b"/test/test", (b"test", b"test"))
+
+if FAILED:
+    sys.exit(1)
