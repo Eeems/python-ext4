1f09da1e6fe2ef75d4107abc07bff836af7b2e2d
Maximilian Knespel <mxmlnkn@users.noreply.github.com>
2025-06-16 06:23:39 +0200

    Fix BlockIO issues (#4)
    * Add missing readable and seekable methods to BlockIO
    
    * Fix BlockIO.seek not returning new offset
    
    ---------
    
    Co-authored-by: Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import sys
 import ext4
 
+from typing import cast
+
 FAILED = False
 
 
-def test_path_tuple(path, expected):
+def test_path_tuple(path: str | bytes, expected: tuple[bytes, ...]):
     global FAILED
     print(f"check Volume.path_tuple({path}): ", end="")
     try:
@@ -21,12 +23,36 @@ def test_path_tuple(path, expected):
         print(e)
 
 
+def _assert(source: str):
+    global FAILED
+    print(f"check {source}: ", end="")
+    if eval(source):
+        print("pass")
+        return
+
+    FAILED = True
+    print("fail")
+
+
 test_path_tuple("/", tuple())
 test_path_tuple(b"/", tuple())
 test_path_tuple("/test", (b"test",))
 test_path_tuple(b"/test", (b"test",))
 test_path_tuple("/test/test", (b"test", b"test"))
 test_path_tuple(b"/test/test", (b"test", b"test"))
+
+with open("test.ext4", "rb") as f:
+    # Extract specific file
+    volume = ext4.Volume(f, offset=0)
+    inode = cast(ext4.File, volume.inode_at("/test.txt"))
+    _assert("isinstance(inode, ext4.File)")
+    b = inode.open()
+    _assert("isinstance(b, ext4.BlockIO)")
+    _assert("b.readable()")
+    _assert("b.seekable()")
+    _assert("b.seek(1) == 1")
+    _assert("b.seek(0) == 0")
+    _assert("b.seek(10) == 10")
 
 if FAILED:
     sys.exit(1)
