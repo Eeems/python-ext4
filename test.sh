e192593a359de77153316ea5187d0b18b72d9848
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2025-08-07 10:23:06 -0600

    Unify test image creation logic (#9)

--- test.sh
+++ test.sh
@@ -13,17 +13,8 @@ python -m pip install wheel
 python -m pip install \
   --extra-index-url=https://wheels.eeems.codes/ \
   -r requirements.txt
-if ! [ -f test.ext4 ];then
-  tmp_dir=$(mktemp -d)
-  trap "rm -r \"$tmp_dir\"" EXIT
-  echo "hello world" > "$tmp_dir"/test.txt
-  for i in {1..100};do
-    echo "hello world" >> "$tmp_dir"/test.txt
-  done
+if ! [ -f test.ext4 ] || ! [ -f test.ext4.tmp ];then
+  ./_test_image.sh
   trap "rm -f test.ext4{,.tmp}" EXIT
-  dd if=/dev/zero of=test.ext4.tmp count=1024 bs=1024
-  mkfs.ext4 test.ext4.tmp -d "$tmp_dir"
-  echo -n F > test.ext4
-  cat test.ext4.tmp >> test.ext4
 fi
 python test.py
