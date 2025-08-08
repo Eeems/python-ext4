72a7e340b820939e71ecd45d5bebf8df7618b792
007gzs <007gzs@gmail.com>
2025-08-08 23:02:41 +0800

    更新 _test_image.sh

--- _test_image.sh
+++ _test_image.sh
@@ -5,12 +5,12 @@ tmp_dir=$(mktemp -d)
 trap "rm -r \"$tmp_dir\"" EXIT
 echo "hello world" > "$tmp_dir"/test.txt
 for i in {1..100};do
+  echo "hello world" >> "$tmp_dir"/test.txt
   echo "hello world$i" >> "$tmp_dir"/test$i.txt
   for j in {1..20};do
     setfattr -n user.name$j -v value${i}_$j "$tmp_dir"/test$i.txt
   done
 done
-rm -r test{32,64}.ext4{,.tmp}
 dd if=/dev/zero of=test32.ext4.tmp count=20 bs=1048576
 dd if=/dev/zero of=test64.ext4.tmp count=20 bs=1048576
 mkfs.ext4 -g 1024 -O 64bit test64.ext4.tmp -d "$tmp_dir"
