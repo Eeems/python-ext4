07f4e74381fe66836379c085a7dc2c4c8fb194df
007gzs <007gzs@gmail.com>
2025-08-08 14:46:07 +0800

    fix rm

--- _test_image.sh
+++ _test_image.sh
@@ -2,6 +2,7 @@
 set -e
 
 tmp_dir=$(mktemp -d)
+trap "rm -r \"$tmp_dir\"" EXIT
 echo "hello world" > "$tmp_dir"/test.txt
 for i in {1..100};do
   echo "hello world$i" >> "$tmp_dir"/test$i.txt
@@ -9,12 +10,11 @@ for i in {1..100};do
     setfattr -n user.name$j -v value${i}_$j "$tmp_dir"/test$i.txt
   done
 done
-trap "rm -r test{32,64}.ext4{,.tmp}" EXIT
+rm -r test{32,64}.ext4{,.tmp}
 dd if=/dev/zero of=test32.ext4.tmp count=20 bs=1048576
 dd if=/dev/zero of=test64.ext4.tmp count=20 bs=1048576
 mkfs.ext4 -g 1024 -O 64bit test64.ext4.tmp -d "$tmp_dir"
 mkfs.ext4 -g 1024 -O ^64bit test32.ext4.tmp -d "$tmp_dir"
-trap "rm -r \"$tmp_dir\"" EXIT
 echo -n F > test32.ext4
 cat test32.ext4.tmp >> test32.ext4
 echo -n F > test64.ext4
